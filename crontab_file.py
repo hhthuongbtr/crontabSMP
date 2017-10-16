fab5d18a5d06327c631a1acfca28cec3e8d84ff7
hhthuongbtr <hhthuongbtr@gmail.com>
2017-10-16 16:18:21 +0700

    add crontab

--- crontab_file.py
+++ crontab_file.py
@@ -1,8 +1,23 @@
 # coding: utf-8
 import subprocess
 import os
+import time
+from datetime import datetime
 
 class Crontab:
+
+    def create(self, date_time, jobid, action):
+        dt=datetime.fromtimestamp(date_time)
+        #print dt
+        DD = dt.day
+        MM = dt.month
+        hh = dt.hour
+        print hh
+        mm = dt.minute
+        ss = dt.second
+        dayofweek = dt.isocalendar()[2]
+        task = """%s %s %s %s %s sleep %s; /bin/python /home/thomson_crontab/start_job/thomson_job.py -j %s -s %s"""%(mm,hh,DD,MM,dayofweek,ss,jobid,action )
+        return task
 
     def _runcmd(self, cmd, input=None):
         if input is not None:
@@ -34,7 +49,7 @@ class Crontab:
             installed_content = ''
         else:
             installed_content =  self.get_list()
-            print installed_content
+            #print installed_content
         installed_crontabs = installed_content.split('\n')
         for crontab in content.split('\n'):
             if crontab and crontab not in installed_crontabs:
@@ -70,5 +85,7 @@ class Crontab:
         if retcode != 0: 
             raise ValueError('failed to install crontab, check if crontab is valid')
 
-Crontab().append(content='11 11 * * * /bin/sh /home/thomson_crontab/add_aa.sh', override=False)
+#Crontab().append(content='11 11 * * * /bin/sh /home/thomson_crontab/add_aa.sh', override=False)
 #Crontab().pop(content='35 15 * * * /bin/sh /home/thomson_crontab/add_aa.sh')
+Crontab().append(Crontab().create(1508144477, '111111', 'start'))
+print Crontab().get_list()
